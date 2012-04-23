2a1eb2f4c779457dd8d14320d59d7e94df641190
jtm <jtm@robot.is>
2012-04-23 19:56:21 +0000

    0.9d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
       name='lesscpy',
-      version='0.9c',
+      version='0.9d',
       description='Lesscss compiler.',
       author='Jóhann T Maríusson',
       author_email='jtm@robot.is',
