7a65e81fa7b9ce0b0997b5fd8b956be36faaf3a7
Sascha Peilicke <saschpe@gmx.de>
2013-08-08 17:44:31 +0200

    Add utility function convergent_round
    Mimics Python3's changed rounding behavior: "The round() function
    rounding strategy and return type have changed. Exact halfway cases are
    now rounded to the nearest even result instead of away from zero. (For
    example, round(2.5) now returns 2 rather than 3.) round(x[, n]) now
    delegates to x.__round__([n]) instead of always returning a float. It
    generally returns an integer when called with a single argument and a
    value of the same type as x when called with two arguments."

--- lesscpy/lessc/utility.py
+++ lesscpy/lessc/utility.py
@@ -11,6 +11,7 @@
 from __future__ import print_function
 
 import collections
+import math
 import re
 
 
@@ -243,3 +244,24 @@ def split_unit(value):
     """
     r = re.search('^(\-?[\d\.]+)(.*)$', str(value))
     return r.groups() if r else ('', '')
+
+
+def convergent_round(value, ndigits=0):
+    """Convergent rounding.
+
+    Round to neareas even, similar to Python3's round() method.
+    """
+    if sys.version_info[0] < 3:
+        if value < 0.0:
+            return -convergent_round(-value)
+
+        epsilon = 0.0000001
+        integral_part, _ = divmod(value, 1)
+
+        if abs(value - (integral_part + 0.5)) < epsilon:
+            if integral_part % 2.0 < epsilon:
+                return integral_part
+            else:
+                nearest_even = integral_part + 0.5
+                return math.ceil(nearest_even)
+    return round(value, ndigits)
