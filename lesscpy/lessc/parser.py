e82ca278c4d2c532ca85bf7c3a1760986254ca4c
Adi Roiban <adi.roiban@chevah.com>
2014-02-18 11:24:00 +0200

    Update after review.

--- lesscpy/lessc/parser.py
+++ lesscpy/lessc/parser.py
@@ -77,23 +77,32 @@ class LessParser(object):
         self.result = None
         self.target = None
 
-    def parse(self, filename='', filestream=None, debuglevel=0):
+    def parse(self, filename=None, file=None, debuglevel=0):
         """ Parse file.
         kwargs:
             filename (str): File to parse
             debuglevel (int): Parser debuglevel
         """
+        self.scope.push()
+
+        if not file:
+            # We use a path.
+            file = filename
+        else:
+            # We use a stream and try to extract the name from the stream.
+            if hasattr(file, 'name'):
+                if filename is not None:
+                    raise AssertionError(
+                        'names of file and filename are in conflict')
+                filename = file.name()
+            else:
+                filename = '(stream)'
+
+        self.target = filename
         if self.verbose:
             print('Compiling target: %s' % filename, file=sys.stderr)
-        self.scope.push()
-        self.target = filename
-        if filestream:
-            content = filestream
-        else:
-            content = filename
-
         self.result = self.parser.parse(
-            content, lexer=self.lex, debug=debuglevel)
+            file, lexer=self.lex, debug=debuglevel)
 
         self.post_parse()
 
