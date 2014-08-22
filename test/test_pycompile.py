66cc5b8ecae568c3a20948718ef2d4f162cfd786
Hannes Gräuler <graeuler@geoplex.de>
2014-08-22 13:45:05 +0200

    Add test if compile() raises an CompilationError

--- test/test_pycompile.py
+++ test/test_pycompile.py
@@ -21,3 +21,13 @@ class TestCompileFunction(unittest.TestCase):
 
         output = compile(StringIO("a { border-width: 2px * 3; }"), minify=True)
         self.assertEqual(output, "a{border-width:6px;}");
+
+    def test_raises_exception(self):
+        """
+        Test if a syntax error raises an exception
+        """
+        from lesscpy.exceptions import CompilationError
+
+        fail_func = lambda: compile(StringIO("a }"), minify=True)
+        self.assertRaises(CompilationError, fail_func)
+
