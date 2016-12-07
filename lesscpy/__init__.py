2ecee092356e485111691449c1175a3996536033
Sergey Komarov <sergey@lumiata.com>
2016-12-07 17:04:52 +0600

    add __main__ module

--- lesscpy/__init__.py
+++ lesscpy/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('0', '12', '1')
+__version_info__ = ('0', '12', '2')
 __version__ = '.'.join(__version_info__)
 
 
--- lesscpy/scripts/compiler.py
+++ lesscpy/scripts/compiler.py
@@ -196,6 +196,4 @@ def run():
                     print(out)
     except (KeyboardInterrupt, SystemExit, IOError):
         sys.exit('\nAborting...')
-        
-if __name__ == '__main__':
-    run()
+
